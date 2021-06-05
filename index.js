4da92e078c0744e3445798a4f16ccc6c67154227
Muhammad Hazmi <mhazmiee@gmail.com>
2021-06-05 10:43:58 +0700

    horo

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const horoskop = require("./routers/horoskop");
-const chapter = require("./routers/chapter");
 const cors = require("cors");
 const helmet = require("helmet");
 
@@ -10,7 +9,6 @@ app.use(cors());
 app.use(helmet());
 app.use("/api", horoskop);
 app.use(express.static("./public"));
-app.use("/api/", chapter);
 app.use("/api", (req, res) => {
   res.send({
     status: true,
